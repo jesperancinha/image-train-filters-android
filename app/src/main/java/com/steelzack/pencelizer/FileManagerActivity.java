ddbf9a5adc88cc50fea8f4923a59f6f769ecd861
Joao Esperancinha <jofisaes@hotmail.com>
2016-02-06 15:47:24 +0100

    SZ-N Completes directory listing on a system level

---
+++ app/src/main/java/com/steelzack/pencelizer/FileManagerActivity.java
@@ -0,0 +1,84 @@
+package com.steelzack.pencelizer;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.steelzack.pencelizer.fileManager.FileManagerItem;
+import com.steelzack.pencelizer.fileManager.FileType;
+
+import java.io.File;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import javax.activation.MimetypesFileTypeMap;
+
+import static com.steelzack.pencelizer.fileManager.FileType.File;
+import static com.steelzack.pencelizer.fileManager.FileType.Folder;
+import static java.util.Collections.*;
+
+/**
+ * Created by joao on 6-2-16.
+ */
+public class FileManagerActivity extends ListActivity{
+    private File currentDirectory;
+    private ArrayAdapter fileManagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        currentDirectory = new File(".");
+    }
+
+    private void createListOfFiles(File directory){
+        final File[] innerFiles = directory.listFiles();
+        final List<FileManagerItem> files = new ArrayList<>();
+        final List<FileManagerItem> directories = new ArrayList<>();
+
+        for(File file : innerFiles)
+        {
+            final FileType type = file.isDirectory() ? Folder : File;
+            final String mimetype= new MimetypesFileTypeMap().getContentType(file);
+            final String fileFormattedDate = getDateString(file);
+            switch (type){
+                case Folder:
+                    directories.add(new FileManagerItem(file.getName(),fileFormattedDate,Folder, file));
+                    break;
+                case File:
+                    if(mimetype.contains(getString(R.string.image_mime_type))) {
+                        files.add(new FileManagerItem(file.getName(), fileFormattedDate, File, file));
+                    }
+                    break;
+            }
+        }
+
+        sort(directories);
+        sort(files);
+        final List<FileManagerItem> parentFolder = Arrays.asList(new FileManagerItem[]{ new FileManagerItem(directory.getName(), getDateString(directory), Folder, directory)});
+        parentFolder.addAll(files);
+        parentFolder.addAll(directories);
+    }
+
+    @NonNull
+    private String getDateString(File file) {
+        final Date lastModDate = new Date(file.lastModified());
+        final DateFormat formater = DateFormat.getDateTimeInstance();
+        return formater.format(lastModDate);
+    }
+
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+    }
+}
+
--- app/src/main/java/com/steelzack/pencelizer/fileManager/FileManagerItem.java
+++ app/src/main/java/com/steelzack/pencelizer/fileManager/FileManagerItem.java
@@ -1,19 +1,33 @@
 package com.steelzack.pencelizer.fileManager;
 
+import java.io.File;
+
 /**
  * Created by joao on 6-2-16.
  */
-public class FileManagerItem {
+public class FileManagerItem implements Comparable<FileManagerItem> {
 
-    final String filename;
+    private final String filename;
 
     final String date;
 
     final FileType fileType;
 
-    public FileManagerItem(String filename, String date, FileType fileType) {
+    final File file;
+
+    public FileManagerItem(String filename, String date, FileType fileType, File file) {
         this.filename = filename;
         this.date = date;
         this.fileType = fileType;
+        this.file = file;
+    }
+
+    @Override
+    public int compareTo(FileManagerItem another) {
+        return another.getFilename().compareTo(this.getFilename());
+    }
+
+    public String getFilename() {
+        return filename;
     }
 }
