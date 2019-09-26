0fea81d7550b5122dc2568610849b1071c490abf
Joao Esperancinha <jofisaes@gmail.com>
2019-09-26 23:11:46 +0200

    QA - Fixes Bettercode scan

--- image-train-filters-app/src/main/java/org/jesperancinha/itf/android/main/MainActivity.java
+++ image-train-filters-app/src/main/java/org/jesperancinha/itf/android/main/MainActivity.java
@@ -28,7 +28,7 @@ import static org.jesperancinha.itf.android.ITFConstants.FILE_FIND;
 import static org.jesperancinha.itf.android.ITFConstants.FOLDER_FIND;
 import static org.jesperancinha.itf.android.R.string.chartizating;
 
-public class MainActivity extends MainActivityManager {
+public class MainActivity extends ActionsMainActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,72 +56,6 @@ public class MainActivity extends MainActivityManager {
             setSelectedOutputFolder(data);
         }
         mainFragment.checkButtonStart();
-    }
-
-    public void pFindFile(View view) {
-        final MainFragment mainFragment = getMainFragment();
-        final Intent intent = new Intent(mainFragment.getActivity(), FileManagerActivity.class);
-        intent.putExtra("directoryManager", false);
-        startActivityForResult(intent, FILE_FIND);
-        mainFragment.checkButtonStart();
-    }
-
-    public void pGenerateFile(View view) {
-        final ControlConfiguration controlConfiguration = mainFragmentManager.getControlConfiguration();
-        controlConfiguration.getBtnStart().setEnabled(false);
-        controlConfiguration.getBtnStartEmail().setEnabled(false);
-        controlConfiguration.getTextStatus().setText(chartizating);
-        final Runnable task = createGenerateImageTask(view, getMainFragment());
-        mainFragmentManager.getControlConfiguration().getTextStatus().post(task);
-    }
-
-    public void pGetBackGroundColor(View view) {
-        final MainFragment mainFragment = getMainFragment();
-        ColorPickerDialogBuilder
-                .with(view.getContext())
-                .setTitle("Choose color")
-                .initialColor(Color.WHITE)
-                .wheelType(ColorPickerView.WHEEL_TYPE.CIRCLE)
-                .density(5)
-                .setPositiveButton("ok", (dialog, selectedColor, allColors) -> {
-                    mainFragmentManager.getControlConfiguration().getSvSelectedColor().setBackgroundColor(selectedColor);
-                    mainFragmentManager.getControlConfiguration().getSvSelectedColor().setColor(selectedColor);
-                })
-                .setNegativeButton("cancel", (dialog, which) -> {
-                })
-                .build().show();
-        mainFragment.checkButtonStart();
-    }
-
-    public void pFindOutputFolder(View view) {
-        final MainFragment mainFragment = getMainFragment();
-        final Intent intent = new Intent(mainFragment.getActivity(), FileManagerActivity.class);
-        intent.putExtra("directoryManager", true);
-        startActivityForResult(intent, FOLDER_FIND);
-        mainFragment.checkButtonStart();
-    }
-
-    public void pAddOne(View view) {
-        final MainFragment mainFragment = getMainFragment();
-        final int currentFontSize = parseInt(mainFragmentManager.getEditConfiguration().getEditFontSize().getText().toString());
-        mainFragmentManager.getEditConfiguration().getEditFontSize().setText(String.valueOf(currentFontSize + 1));
-        mainFragment.checkButtonStart();
-    }
-
-    public void pMinusOne(View view) {
-        final MainFragment mainFragment = getMainFragment();
-        final int currentFontSize = parseInt(mainFragmentManager.getEditConfiguration().getEditFontSize().getText().toString());
-        mainFragmentManager.getEditConfiguration().getEditFontSize().setText(String.valueOf(currentFontSize - 1));
-        mainFragment.checkButtonStart();
-    }
-
-    public void pSendEmail(View view) {
-        final MainFragment mainFragment = (MainFragment) getSupportFragmentManager().getFragments().get(0);
-        final EmailFragment emailFragment = (EmailFragment) getSupportFragmentManager().getFragments().get(1);
-        final String outputFileName = mainFragmentManager.getOutputFileName();
-        final MailSender mailSender = MailSender.builder().mainFragment(mainFragment).emailFragment(emailFragment)
-                .outputFileName(outputFileName).build();
-        mailSender.sendEmail();
     }
 
 }
